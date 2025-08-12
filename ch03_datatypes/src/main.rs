400618b584a8dd198a6818d60bba022c26ddf690
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-08-12 18:17:25 +0530

    added ch3

---
+++ ch03_datatypes/src/main.rs
@@ -0,0 +1,53 @@
+// 1- numbers 
+// fn main(){
+//     let x:u32 = 1;
+//     println!("{}",x)
+// }
+
+
+
+// 2- booleans 
+
+// fn main(){
+//     let is_male = false ;
+//     let is_above_18 = true ;
+
+
+//     if is_male{
+//         println!("you are a male !");
+//     }else{
+//         println!("you are not a  male ");
+
+//     }
+
+//     if is_male&& is_above_18{
+//         println!("you are a legal male ");
+//     }
+// }
+
+
+
+// 3- string 
+
+// fn main(){
+//     let greeting:String = String::from("hello everyone ");
+//     println!("{}" , greeting);
+// }
+
+
+
+// 4- Arrays 
+
+// fn main(){
+//     let arr:[i32 ; 5] = [1,2,3,4,5] ;
+//     println!("{:?}", arr);
+// }
+
+
+// 5- vectors 
+
+fn main(){
+    let vector  = vec![1,2,3];
+    println!("{:?}" , vector );
+
+}
