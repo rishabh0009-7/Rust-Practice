8406860ac070a0dc88b6e0e6d47e118ed4dad435
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-08-16 12:49:15 +0530

    implementing structs

--- ch08_structs/src/main.rs
+++ ch08_structs/src/main.rs
@@ -92,30 +92,60 @@
 
 
 // add the trait clone  so now  it is clone and it will work 
-#[derive(Clone)]
-struct User{
-    active:bool, 
-    sign_in_count:u64,
-    username : String
+// in strng you cant use copy trait so use clone 
+// #[derive(Clone)]
+// struct User{
+//     active:bool, 
+//     sign_in_count:u64,
+//     username : String
+// }
+
+
+
+// fn main(){
+//     let mut user1 = User {
+//         active:true ,
+//         sign_in_count :1,
+//         username:String::from("raa")
+//     };
+
+//     name(user1.clone());
+//     print!("User 1 username: {}", user1.username); //Error - can not use borrowed value
+
+    
+// }
+
+
+// fn name(user1:User){
+//     print!("User 1 username: {}", user1.active);
+
+// }
+
+
+
+// implementing struct 
+// Passing in &self as the first argument to a function
+
+
+struct Rect{
+    width:u32,
+    height:u32,
 }
 
+impl Rect {
+    fn area (&self)->u32{
+        self.width * self.height
+    }
+}
 
 
 fn main(){
-    let mut user1 = User {
-        active:true ,
-        sign_in_count :1,
-        username:String::from("raa")
+    let Rect1 = Rect {
+        width : 30,
+        height : 50
     };
 
-    name(user1.clone());
-    print!("User 1 username: {}", user1.username); //Error - can not use borrowed value
 
-    
-}
-
-
-fn name(user1:User){
-    print!("User 1 username: {}", user1.active);
+    println!(" The area of rectangle is {}" , Rect1.area());
 
 }
