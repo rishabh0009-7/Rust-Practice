07367196724e0510c93234708785468321f9d840
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-08-17 13:18:56 +0530

    error handling in rust

---
+++ ch10_errorhandling/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+fn main(){
+    let greeting_result:Result<String, std::io::Error> = fs::read_to_string("hello.txt");
+
+
+    match greeting_result{
+        Ok(file_content )=>{
+            println!("file created sucessfully {:?}" , file_content)
+        },
+        Err(error)=>{
+            println!("error {:?}", error)
+        }
+    }
+}
