76384ba4f44eb28e10ad27557fae8ef70fff3474
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-08-18 13:28:59 +0530

    generics over struct and enums

--- ch13_generics_traits/src/main.rs
+++ ch13_generics_traits/src/main.rs
@@ -24,16 +24,122 @@
 
 //"This generic type T must have certain abilities (traits) so I can use them." 
 
-fn biggest_element<T: PartialOrd>(a: T, b: T) -> T {
-    if a > b {
-        return a;
-    }
-    return b;
+// fn biggest_element<T: PartialOrd>(a: T, b: T) -> T {
+//     if a > b {
+//         return a;
+//     }
+//     return b;
+// }
+
+// fn main() {
+//     println!("{}", biggest_element(10, 20));         // works with i32
+//     println!("{}", biggest_element(3.5, 2.1));       // works with f64
+//     println!("{}", biggest_element('a', 'z'));       // works with char
+//     println!("{}", biggest_element("Rust", "Go"));   // works with &str
+// }
+
+
+
+// generic over structs 
+
+// without generic 
+
+
+// struct Rect{
+//     width :u32,
+//     height:u32,
+
+// }
+
+// impl Rect{
+//     fn area(&self)->u32{
+//         self.width*self.height
+
+
+
+//     }
+// }
+
+// fn main(){
+//     let r = Rect{
+//         width:20,
+//         height:30,
+
+//     };
+
+//     println!("{}" , r.area());
+// }
+
+
+// with generic 
+
+// use std::ops::Mul;
+
+// #[derive(Debug)]
+// struct Rect<T> {
+//     width: T,
+//     height: T,
+// }
+
+// impl<T: Mul<Output = T> + Copy> Rect<T> {
+//     fn area(&self) -> T {
+//         self.width * self.height
+//     }
+// }
+
+// fn main() {
+//     let r = Rect {
+//         width: 20,
+//         height: 30,
+//     };
+
+//     println!("Area = {}", r.area());
+// }
+
+
+// generic over enums 
+
+
+// enum MyOption<T> {
+//     Some(T),  // holds a value of type T
+//     None,     // no value
+// }
+
+// fn main() {
+//     let a: MyOption<i32> = MyOption::Some(42);
+//     let b: MyOption<i32> = MyOption::None;
+
+//     match a {
+//         MyOption::Some(v) => println!("Got {}", v),
+//         MyOption::None => println!("Got nothing"),
+//     }
+
+//     match b {
+//         MyOption::Some(v) => println!("Got {}", v),
+//         MyOption::None => println!("Got nothing"),
+//     }
+// }
+
+
+
+
+enum MyResult<T, E> {
+    Ok(T),
+    Err(E),
 }
 
 fn main() {
-    println!("{}", biggest_element(10, 20));         // works with i32
-    println!("{}", biggest_element(3.5, 2.1));       // works with f64
-    println!("{}", biggest_element('a', 'z'));       // works with char
-    println!("{}", biggest_element("Rust", "Go"));   // works with &str
+    let success: MyResult<i32, &str> = MyResult::Ok(200);
+    let failure: MyResult<i32, &str> = MyResult::Err("Something went wrong");
+
+    match success {
+        MyResult::Ok(v) => println!("Success: {}", v),
+        MyResult::Err(e) => println!("Error: {}", e),
+    }
+
+    match failure {
+        MyResult::Ok(v) => println!("Success: {}", v),
+        MyResult::Err(e) => println!("Error: {}", e),
+    }
 }
+
