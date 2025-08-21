32a3082de900333bb8dc2dc917b54d3d0f801977
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-08-21 19:18:53 +0530

    lifetimes more

--- ch20_Lifetimes/src/main.rs
+++ ch20_Lifetimes/src/main.rs
@@ -25,20 +25,90 @@
 
 // When functions take references, Rust sometimes cannot infer how long references should live.
 // We annotate with 'a (a lifetime parameter).
- fn lonegest(s1:&'a str , s2:& 'a str) {
-    if s1.len() > s2.len() {
-        s1
-    } else{
-        s2
-    }
+//  fn lonegest<'a>(s1:&'a str , s2:& 'a str) -> &'a str{
+//     if s1.len() > s2.len() {
+//         s1
+//     } else{
+//         s2
+//     }
 
- }
+//  }
 
 
-fn main(){
-    let s1 = String::from("hello ");
-    let s2  = "world"
+// fn main(){
+//     let s1 = String::from("hello ");
+//     let s2  = "world";
 
-    let result = lonegest(s1.as_str() , s2)
-    println!("{}" , result)
+//     let result = lonegest(s1.as_str() , s2);
+//     println!("{}" , result);
+// }
+
+
+// Lifetime Elision (when you don’t need 'a)
+// if there is only input reference there is no need to write  'a
+
+
+// fn first_word (s:&str) -> &str{
+//     &s[..1]
+
+
+// }
+
+// fn main (){
+//     let s = String::from ("hello")
+//     let result = first_word(&s);
+//     println!("{}" , result)
+// }
+
+
+// Structs with Lifetimes
+
+// If a struct holds references, it must carry lifetime annotations.
+
+// struct Book <'a>{
+//     title:&'a str,
+//     author: &'a str,
+
+
+
+// }
+
+// fn main(){
+//     let title = String::from("Rust in action ");
+//     let author = String::from ("Tim McNamara");
+
+//     let book = Book{
+//         title : &title ,
+//         author : &author,
+//     };
+
+//     println!("{} by {}" , book.title , book.author);
+
+
+    
+// }
+
+
+
+
+// Multiple Lifetimes
+
+// You can have multiple lifetime parameters if references are unrelated.
+
+
+fn mix <'a,'b>( x:&'a str , y :&'b str) -> (&'a str , &'b str) {
+    (x,y)
+
+
 }
+fn main (){
+    let s1 = String::from ("hello ");
+    let s2 = String::from ("world");
+
+    let (a, b )= mix(&s1 , &s2) ;
+    println!( "a = {} , b= {}" , a,b);
+
+
+}
+
+
